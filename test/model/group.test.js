1aa02d94562e205dac34cd33f9f13551ec4553dc
muzzynine <muzzynine@bigfrogs.co.kr>
2016-03-21 23:59:28 +0900

    added test suite for membershipController

---
+++ test/model/group.test.js
@@ -0,0 +1,8 @@
+var Group = require('../../model_migration/group');
+
+describe('group - group model dao', function(){
+    describe('#createGroup', function(){
+	it('It works for me', function(){
+	});
+    });
+});
